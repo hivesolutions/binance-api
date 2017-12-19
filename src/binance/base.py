9a3904ddc172db3be03bc4c3bd7bacacf83fd123
joamag <joamag@hive.pt>
2017-12-19 15:23:10 +0000

    initial working version

---
+++ src/binance/base.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Binance API
+# Copyright (c) 2008-2017 Hive Solutions Lda.
+#
+# This file is part of Hive Binance API.
+#
+# Hive Binance API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Binance API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Binance API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2017 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import appier
+
+from . import ticker
+
+BASE_URL = "https://api.binance.com/api/v1/"
+""" The default base URL to be used when no other
+base url value is provided to the constructor """
+
+class API(
+    appier.API,
+    ticker.TickerAPI
+):
+
+    def __init__(self, *args, **kwargs):
+        appier.API.__init__(self, *args, **kwargs)
+        self.base_url = appier.conf("BINANCE_BASE_URL", BASE_URL)
+        self.api_key = appier.conf("BINANCE_API_KEY", None)
+        self.secret = appier.conf("BINANCE_SECRET", None)
+        self.base_url = kwargs.get("base_url", self.base_url)
+        self.api_key = kwargs.get("api_key", self.api_key)
+        self.secret = kwargs.get("secret", self.secret)
+
+    def build(
+        self,
+        method,
+        url,
+        data = None,
+        data_j = None,
+        data_m = None,
+        headers = None,
+        params = None,
+        mime = None,
+        kwargs = None
+    ):
+        pass
---
+++ src/examples/app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Binance API
+# Copyright (c) 2008-2017 Hive Solutions Lda.
+#
+# This file is part of Hive Binance API.
+#
+# Hive Binance API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Binance API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Binance API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2017 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import appier
+
+import base
+
+class BinanceApp(appier.WebApp):
+
+    def __init__(self, *args, **kwargs):
+        appier.WebApp.__init__(
+            self,
+            name = "binance",
+            *args, **kwargs
+        )
+
+    @appier.route("/", "GET")
+    def index(self):
+        return self.stacks()
+
+    @appier.route("/ticker", "GET")
+    def ticker(self):
+        api = self.get_api()
+        ticker = api.all_ticker()
+        return ticker
+
+    def get_api(self):
+        api = base.get_api()
+        return api
+
+if __name__ == "__main__":
+    app = BinanceApp()
+    app.serve()
+else:
+    __path__ = []
